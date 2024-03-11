2e1d4e664bc9c646e87bf28dab2af358c27b4873
RISHABHKOCHAR27 <rishabhkochar27@gmail.com>
2024-03-12 00:23:50 +0530

    Added section titles and section index. Changes the fetchImageFromAPI function to accept url directly.

--- Assignment4- phone contact app/contactListViewController.swift
+++ Assignment4- phone contact app/contactListViewController.swift
@@ -15,6 +15,7 @@ class contactListViewController: UIViewController {
     
     var sectionDict = [String: [Contacts]]()
     
+    @IBOutlet weak var newContactButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,29 +23,25 @@ class contactListViewController: UIViewController {
         
         let nib = UINib(nibName: "contactTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "contactTableViewCell")
+        self.tableView.rowHeight = 93.0
+
         
         contactListApi(URL: "https://61db84524593510017aff8d4.mockapi.io/contactbook/v1/getlist") { result in
             self.data = result
+            self.populateSections()
             DispatchQueue.main.async {      //update ui on the main thread
                 self.tableView.reloadData()
             }
         }
         
-        sectionTitle = Array(Set(data.compactMap({String($0.firstName.prefix(1))})))  //take the first character of names
-        sectionTitle.sort()
-        sectionTitle.forEach({sectionDict[$0] = [Contacts]()}) // make blank array for every section title
-       // data.forEach({sectionDict[String($0.firstName.prefix(1))]?.append($0)})
-        data.forEach { contact in
-                let firstLetter = String(contact.firstName.prefix(1))
-                if var contactsArray = sectionDict[firstLetter] {
-                    contactsArray.append(contact)
-                    sectionDict[firstLetter] = contactsArray
-                } else {
-                    sectionDict[firstLetter] = [contact]
-                }
-            }
-        
     }
+        
+        func populateSections() {
+            sectionTitle = Array(Set(data.map { String($0.firstName.prefix(1)) }))
+            sectionTitle.sort()
+            sectionDict = Dictionary(grouping: data) { String($0.firstName.prefix(1)) }
+        }
+        
 
     func contactListApi(URL url: String, completion: @escaping ([Contacts]) -> Void){  // @escaping has 2 purposes: storing a closure and async operating purposes.
         
@@ -66,15 +63,13 @@ class contactListViewController: UIViewController {
     
     //this part can be done in other ways - ask Prasanna
     
-    func fetchImageFromAPI(for indexPath: IndexPath, completion: @escaping (UIImage?) -> Void) {
-            
-            let imageURLString = data[indexPath.row].avatarUrl
-            
-            // Convert the imageURLString to URL
-            guard let imageURL = URL(string: imageURLString) else {
-                completion(nil)
-                return
-            }
+    // made the function to acept the url directly so that each cell will fetch its corresponding image.
+    
+    func fetchImageFromAPI(url: String, completion: @escaping (UIImage?) -> Void) {
+        guard let imageURL = URL(string: url) else {
+            completion(nil)
+            return
+        }
             
             // Fetch image data asynchronously
             URLSession.shared.dataTask(with: imageURL) { data, response, error in
@@ -96,35 +91,56 @@ class contactListViewController: UIViewController {
 extension contactListViewController: UITableViewDataSource, UITableViewDelegate{
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        sectionTitle.count
+        return sectionTitle.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // return data.count
-        
-        //DOUBT
-        sectionDict[sectionTitle[section]]!.count
+    
+         return sectionDict[sectionTitle[section]]?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "contactTableViewCell") as! contactTableViewCell
-        //cell.firstName.text = data[indexPath.row].firstName
-        cell.firstName.text = sectionDict[sectionTitle[indexPath.section]]?[indexPath.row].firstName
-        cell.lastName.text = data[indexPath.row].lastName
-        cell.lastName.text = sectionDict[sectionTitle[indexPath.section]]?[indexPath.row].lastName
-       // cell.contactImage.image = UIImage(data[indexPath.row].avatarUrl)
-        fetchImageFromAPI(for: indexPath) { image in
-                    // Update UI on the main thread
-                    DispatchQueue.main.async {
-                        cell.contactImage.image = image
-                    }
-                }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactTableViewCell", for: indexPath) as! contactTableViewCell
         
+        
+        if let contact = sectionDict[sectionTitle[indexPath.section]]?[indexPath.row] {
+                    let fullName = "\(contact.firstName) \(contact.lastName)"
+                    cell.firstName.text = fullName
+                
+                // Fetch contact image asynchronously
+                    fetchImageFromAPI(url: contact.avatarUrl) { image in
+                        DispatchQueue.main.async {
+                            // Check if the cell is still visible and if the fetched image is not nil
+                            if let visibleIndexPaths = tableView.indexPathsForVisibleRows, visibleIndexPaths.contains(indexPath), let image = image {
+                                // Assign the fetched image to the cell
+                                cell.contactImage.image = image
+                            }
+                        }
+                    }
+                    
+                }
+
+//        // cell.firstName.text = sectionDict[sectionTitle[indexPath.section]]?[indexPath.row].firstName
+//        //cell.lastName.text = data[indexPath.row].lastName
+//       // cell.lastName.text = sectionDict[sectionTitle[indexPath.section]]?[indexPath.row].lastName
+//       // cell.contactImage.image = UIImage(data[indexPath.row].avatarUrl)
+//
+//
+//        fetchImageFromAPI(for: indexPath) { image in
+//                    // Update UI on the main thread
+//                    DispatchQueue.main.async {
+//                        cell.contactImage.image = image
+//                    }
+//                }
         return cell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        sectionTitle[section]
+        return sectionTitle[section]
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        sectionTitle
     }
 }
 
